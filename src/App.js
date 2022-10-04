1cfa765ff217db3cbc47fb70f6370159d2598a31
azkinf16 <azkifajri16@gmail.com>
2022-10-04 08:17:13 +0700

    adding navigation and home components in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
+
+// import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
-      halo
+      <Navigation />
+      <Home />
     </div>
   );
 }
