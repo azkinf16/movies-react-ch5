43123d7f2c9e15aa42e72296b35455d55d9f9ae0
azkinf16 <azkifajri16@gmail.com>
2022-10-04 22:21:08 +0700

    fixing search bar style

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,16 +8,16 @@ function Navigation() {
         src="https://movielist-react-app.netlify.app/static/media/Logo.eeba5c17ddf85f2145e83dd963662921.svg"
         alt="logo"
       />
-      <div className="bg-transparent border-solid border-2 border-slate-500 rounded-full hover:border-red-400 hover:ring-1 flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+      <div className="bg-transparent border-solid border-2 border-white rounded-full hover:border-red-300 hover:ring-1 flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
         <input
-          className="bg-transparent p-2 w-full focus:outline-none placeholder:text-slate-500"
+          className="bg-transparent p-2 w-full focus:outline-none placeholder:text-white text-white"
           type="text"
           placeholder="What do you want to watch?"
         />
-        <AiOutlineSearch size={25} style={{ color: "#F5B6B7" }} />
+        <AiOutlineSearch size={25} style={{ color: "#FFFF" }} />
       </div>
       <div>
-        <button className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded-full mr-2">
+        <button className="bg-transparent hover:bg-red-500 text-red-500 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded-full mr-2">
           Login
         </button>
         <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">
