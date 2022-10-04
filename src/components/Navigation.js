d709261c599cceddc715a3ff819e303c1f920464
azkinf16 <azkifajri16@gmail.com>
2022-10-04 08:32:21 +0700

    fixing nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,14 +3,14 @@ import { AiOutlineSearch } from "react-icons/ai";
 
 function Navigation() {
   return (
-    <div className="flex items-center justify-between p-4 z-10 absolute w-full">
+    <div className="flex items-center justify-between py-5 px-10 z-10 absolute w-full">
       <img
         src="https://movielist-react-app.netlify.app/static/media/Logo.eeba5c17ddf85f2145e83dd963662921.svg"
         alt="logo"
       />
-      <div className="bg-transparent border-solid border-2 border-gray-400/50 rounded-full hover:ring-red-300 hover:ring-1 flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+      <div className="bg-transparent border-solid border-2 border-slate-500 rounded-full hover:border-red-400 hover:ring-1 flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
         <input
-          className="bg-transparent p-2 w-full focus:outline-none"
+          className="bg-transparent p-2 w-full focus:outline-none placeholder:text-slate-500"
           type="text"
           placeholder="What do you want to watch?"
         />
