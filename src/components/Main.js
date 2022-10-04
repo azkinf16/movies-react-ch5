e7dcbb8f66870f69477033a601145d0975076fae
azkinf16 <azkifajri16@gmail.com>
2022-10-04 19:15:36 +0700

    fixing UI on main header page

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,7 +17,10 @@ function Main() {
       });
   }, []);
 
-  console.log(movies);
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+  const movie2 = movies[Math.floor(Math.random() * movies.length)];
+  const movie3 = movies[Math.floor(Math.random() * movies.length)];
+  console.log("movies", movies);
 
   return (
     <>
@@ -26,7 +29,7 @@ function Main() {
         className="carousel slide relative"
         data-bs-ride="carousel"
       >
-        <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+        <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center pb-4">
           <button
             type="button"
             data-bs-target="#carouselExampleCaptions"
@@ -48,27 +51,63 @@ function Main() {
             aria-label="Slide 3"
           ></button>
         </div>
-        <div className="carousel-inner relative w-full overflow-hidden">
-          <div className="carousel-item active float-left w-full">
+        <div class="carousel-inner relative w-full overflow-hidden">
+          <div class="carousel-item active float-left w-full">
+            <div className="absolute w-3/12 h-screen bg-gradient-to-r from-black"></div>
             <img
-              src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg"
-              className="block w-full h-screen"
-              alt="..."
+              src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+              class="block w-full h-screen object-cover"
+              alt={movie?.title}
             />
+            <div className="absolute w-1/2 top-[25%] p-5">
+              <h1 className="text-8xl font-semibold text-white">
+                {movie?.title}
+              </h1>
+              <p className="text-white font-normal pt-5 pl-1">
+                {movie?.overview}
+              </p>
+              <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mt-10 rounded-full">
+                Watch Trailer
+              </button>
+            </div>
           </div>
-          <div className="carousel-item relative float-left w-full">
+          <div class="carousel-item float-left w-full">
+            <div className="absolute w-3/12 h-screen bg-gradient-to-r from-black"></div>
             <img
-              src="https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg"
-              className="block w-full h-screen"
-              alt="..."
+              src={`https://image.tmdb.org/t/p/original/${movie2?.backdrop_path}`}
+              class="block w-full h-screen object-cover"
+              alt={movie2?.title}
             />
+            <div className="absolute w-1/2 top-[25%] p-5">
+              <h1 className="text-8xl font-semibold text-white w-full">
+                {movie2?.title}
+              </h1>
+              <p className="text-white font-normal pl-1 pt-5">
+              {movie2?.overview}
+              </p>
+              <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mt-10 rounded-full">
+                Watch Trailer
+              </button>
+            </div>
           </div>
-          <div className="carousel-item relative float-left w-full">
+          <div class="carousel-item float-left w-full">
+            <div className="absolute w-3/12 h-screen bg-gradient-to-r from-black"></div>
             <img
-              src="https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg"
-              className="block w-full h-screen"
-              alt="..."
+              src={`https://image.tmdb.org/t/p/original/${movie3?.backdrop_path}`}
+              class="block w-full h-screen object-cover"
+              alt={movie3?.title}
             />
+            <div className="absolute w-1/2 top-[25%] p-5">
+              <h1 className="text-8xl font-semibold text-white w-full">
+                {movie3?.title}
+              </h1>
+              <p className="text-white font-normal pl-1 pt-5">
+              {movie3?.overview}
+              </p>
+              <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mt-10 rounded-full">
+                Watch Trailer
+              </button>
+            </div>
           </div>
         </div>
       </div>
