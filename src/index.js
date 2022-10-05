da8e4c5eda9bcb72553ec70357f3c74b71820e9d
azkinf16 <azkifajri16@gmail.com>
2022-10-06 02:16:36 +0700

    adding routes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'tw-elements';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
