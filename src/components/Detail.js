58c369d184126c351001901f372756b2edd96bd3
azkinf16 <azkifajri16@gmail.com>
2022-10-06 10:14:22 +0700

    fixing section on detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,11 +2,20 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+
+import { FreeMode } from "swiper";
+
 import { AiOutlinePlayCircle } from "react-icons/ai";
 import { BsStar } from "react-icons/bs";
 
 function Detail() {
   const [details, setDetails] = useState([]);
+  const [cast, setCast] = useState([]);
+  const [review, setReview] = useState([]);
   const { id } = useParams();
 
   const API_URL = "https://api.themoviedb.org/3";
@@ -26,46 +35,147 @@ function Detail() {
     }
   };
 
+  const getCasts = async () => {
+    try {
+      const res = await axios.get(`${API_URL}/movie/${id}/credits`, {
+        params: {
+          api_key: API_KEY,
+        },
+      });
+
+      setCast(res.data.cast.slice(0, 10));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getReviews = async () => {
+    try {
+      const res = await axios.get(`${API_URL}/movie/${id}/reviews`, {
+        params: {
+          api_key: API_KEY,
+        },
+      });
+
+      setReview(res.data.results.slice(0, 2));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getDetails();
+    getCasts();
+    getReviews();
   }, []);
 
-  console.log(details);
-  console.log(details.vote_average);
-
-  console.log(Math.round(details.vote_average));
+  console.log("details", details);
+  console.log("cast", cast);
+  console.log("reviews", review);
 
   return (
     <>
-      <div className="absolute w-1/2 h-screen bg-gradient-to-r from-black"></div>
-      <img
-        src={`https://image.tmdb.org/t/p/original/${details.backdrop_path}`}
-        className="block w-full h-screen object-cover"
-        alt=""
-      />
-      <div className="absolute w-1/2 top-[25%] p-5 ml-5">
-        <h1 className="text-5xl font-bold text-white">{details.title}</h1>
-        <div className="flex pt-4">
-          {details.genres &&
-            details.genres.map((item, id) => {
-              return (
-                <p className="text-white mr-2 pl-1 font-light" key={id}>
-                  {item.name}
-                </p>
-              );
-            })}
+      <div className="header">
+        <div className="absolute w-1/2 h-screen bg-gradient-to-r from-black"></div>
+        <img
+          src={`https://image.tmdb.org/t/p/original/${details.backdrop_path}`}
+          className="block w-full h-screen object-cover"
+          alt={details.title}
+        />
+        <div className="absolute w-1/2 top-[25%] p-5 ml-5">
+          <h1 className="text-5xl font-bold text-white">{details.title}</h1>
+          <div className="flex pt-4">
+            {details.genres &&
+              details.genres.map((item, id) => {
+                return (
+                  <p className="text-white mr-2 pl-1 font-light" key={item.id}>
+                    {item.name}
+                  </p>
+                );
+              })}
+          </div>
+          <p className="text-white text-base font-normal pt-8 pl-1">
+            {details.overview}
+          </p>
+          <div className="flex pt-5 pl-1 text-base items-center">
+            <BsStar className="text-yellow-500" />
+            <p className="text-white ml-4">
+              {Math.round(details.vote_average)} / 10
+            </p>
+          </div>
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-5 ml-1 mt-10 rounded-full inline-flex items-center">
+            <AiOutlinePlayCircle className="mr-2" />
+            <span>Watch Trailer</span>
+          </button>
         </div>
-        <p className="text-white text-base font-normal pt-8 pl-1">
-          {details.overview}
-        </p>
-        <div className="flex pt-5 pl-1 text-base items-center">
-          <BsStar className="text-yellow-500" />
-          <p className="text-white ml-4">{Math.round(details.vote_average)} / 10</p>
-        </div>
-        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-5 ml-1 mt-10 rounded-full inline-flex items-center">
-          <AiOutlinePlayCircle className="mr-2" />
-          <span>Watch Trailer</span>
-        </button>
+      </div>
+      <div className="pt-16 mx-11 flex items-center justify-between">
+        <h1 className="text-3xl font-bold">Cast</h1>
+      </div>
+      <div className="py-16 mx-6">
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={30}
+          freeMode={true}
+          modules={[FreeMode]}
+          className="mySwiper"
+        >
+          {cast.length >= 0 &&
+            cast.map((item) => (
+              <SwiperSlide key={item.cast_id}>
+                <div className="w-full inline-block cursor-pointer relative p-2">
+                  <img
+                    className="w-full h-full rounded-lg"
+                    src={`https://image.tmdb.org/t/p/original/${item.profile_path}`}
+                    alt={item.name}
+                  />
+                  <h1 className="pt-4 pb-1 font-bold text-xl">{item.name}</h1>
+                  <h2 className="text-sm text-gray-500">{item.character}</h2>
+                </div>
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </div>
+      <div className="pt-8 mx-11 pb-7 text-center">
+        <h1 className="text-3xl font-bold">What People Says ?</h1>
+      </div>
+      <div className="mx-10 pt-10 pb-10 flex justify-evenly">
+        {review.length >= 0 &&
+          review.map((item) => (
+            <div className="max-w-lg h-full">
+              <div className="border border-gray-400 rounded-lg p-4 flex flex-col justify-between leading-normal">
+                <div className="mb-8">
+                  <p className="text-sm text-gray-600 flex items-center">
+                    <BsStar className="text-yellow-500" />
+                    <p className="ml-2 pb-0.5">
+                      {item.author_details.rating} / 10
+                    </p>
+                  </p>
+                  <div className="text-gray-900 font-bold text-xl my-3">
+                    <p>{item.author}</p>
+                  </div>
+                  <p className="text-gray-700 text-xs text-justify">
+                    {item.content}
+                  </p>
+                </div>
+                <div className="flex items-center">
+                  <img
+                    className="w-10 h-10 rounded-full mr-4"
+                    src="https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector-PNG-File.png"
+                    alt=""
+                  />
+                  <div className="text-sm">
+                    <p className="text-gray-900 leading-none">
+                      {item.author_details.name}
+                    </p>
+                    <p className="text-gray-600 text-xs">
+                      Release at : {item.created_at}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
       </div>
     </>
   );
